1ac0e80ee64fef545d9276437942b01cb0b0c719
Donika Fetahu <fetahudonike@gmail.com>
2021-10-14 14:06:34 +0200

    A message from the universe

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,7 +36,7 @@ public class TestGreeter {
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you making me Happy";
+    String someone = "A message from the universe";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
